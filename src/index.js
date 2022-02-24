d4133474050666b07a0a69b893d42eb60f2047f3
Gyaneshwar Kumar <codewithgyan@gmail.com>
2022-02-25 01:06:55 +0530

    multiple file upload feature added

--- src/index.js
+++ src/index.js
@@ -3,8 +3,9 @@ const express = require('express');
 const app=express();
 var cors = require('cors')
 const multer = require('multer')
-const AWS = require('aws-sdk')
+const aws = require('aws-sdk')
 const uuid = require('uuid/v4')
+const multerS3 = require("multer-s3");
 
 app.use(express.json());
 app.use(express.static("public"))
@@ -18,19 +19,13 @@ app.use("images",express.static(__dirname+"public/images"))
 app.set('views', './public/')
 app.set('view engine', 'ejs')
 
-const s3 = new AWS.S3({
-    accessKeyId: process.env.AWS_ID,
-    secretAccessKey: process.env.AWS_SECRET
-})
+aws.config.update({
+    accessKeyId: process.env.AWS_ACCESS_ID,
+    secretAccessKey: process.env.AWS_SECRET_KEY,
+    region:"ap-south-1"
+});
 
-const storage = multer.memoryStorage({
-    destination: function(req, file, callback) {
-        callback(null, '')
-    }
-})
-
-const upload = multer({storage}).single('mypic')
-
+const s3 = new aws.S3();
 
 app.post("/login",(req,res)=>{
         if(req.body.email=="gyan@gmail.com" && req.body.password=="gyan")
@@ -40,32 +35,49 @@ app.post("/login",(req,res)=>{
         else
         {
              res.status(500).render("index",{status:false,authenticate:false});
-        }
-           
+        }        
 });
 app.get("/upload",(req,res)=>{
     res.render("upload");
 })
 
-app.post("/uploadfile",upload,function (req, res) {
-    let myFile = req.file.originalname.split(".")
-    const fileType = myFile[myFile.length - 1]
+app.post("/uploadfile",function (req, res) {
+    let upload = multer({
+		storage: multerS3({
+			s3: s3,
+			acl: 'public-read',
+			bucket: process.env.AWS_BUCKET_NAME,
+			key: function(req, file, cb){
+				cb(null, `${uuid()}`+file.originalname);
+			}
+		}),
+		fileFilter: function(req, file, cb){
+			const fileExt = file.mimetype.split("/")[1];
+			const extArray = ['jpeg', 'jpg', 'png', 'gif','webp'];
+			const error = new Error("File type not support");
 
-    const params = {
-        Bucket: process.env.AWS_BUCKET_NAME,
-        Key: `${uuid()}.${fileType}`,
-        Body: req.file.buffer
-    }
+			if(!file) res.status(500).render("error");
+			
+			if(extArray.includes(fileExt)){
+				cb(null, true);
+			} else {
+				cb(error, false);
+			};
+		},
+		limits: {fileSize: 2*1024*1024}
+	}).fields([{ name: 'file1', maxCount: 1 }, { name: 'file2', maxCount: 1 }]);
 
-    s3.upload(params, (error, data) => {
-        if(error){
-            res.status(500).render("error");
-        }
-
-        res.status(200).render("success");
-    })
- 
+	upload(req, res, (err) =>{
+		if(err instanceof multer.MulterError || err){
+			console.log("UPLOD-Error: ",err);
+			return res.status(400).render("error");
+		};
+		
+		res.status(200).render("success");
+	});
 })
+
+
 app.get("/",(req,res)=>{
     res.render("index");
 })
